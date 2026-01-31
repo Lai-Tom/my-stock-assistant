e0c92b31d9cf55423d495a6882f4a67eec44b59f
Lai-Tom <diablo9778@gmail.com>
2026-02-01 00:34:15 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
 // 這段程式碼負責把 App 元件畫到網頁上的 "root" 區塊
 ReactDOM.createRoot(document.getElementById('root')).render(
